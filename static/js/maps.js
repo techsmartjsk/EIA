1d8b6356713d6b1d5d86300e29d78039437fce35
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-07-28 19:17:28 +0530

    Done

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,21 +1,19 @@
 function ToggleFunction(){
-    alert('Hey!!!');
-    if(document.getElementById('sidebar-right').style.display === 'none'){
-        document.getElementById('left').style.marginRight = '370px';
-        document.getElementById('sidebar-right').style.display = 'block';
+    if(document.getElementById('sidebar-right').style.display === "none"){
+        document.getElementById('left').style.marginRight = "370px";
+        document.getElementById('sidebar-right').style.display = "block";
     }else{
-        document.getElementById('left').style.marginRight = '30px';
-        document.getElementById('sidebar-right').style.display = 'none';
+        document.getElementById('left').style.marginRight = "30px";
+        document.getElementById('sidebar-right').style.display = "none";
     }
 }
 
 function RightToggleFunction(){
-    alert('Hey!!!');
-    if(document.getElementById('sidebar-left').style.display === 'none'){
-        document.getElementById('right').style.marginLeft = '170px';
-        document.getElementById('sidebar-left').style.display = 'flex';
+    if(document.getElementById('sidebar-left').style.display === "none"){
+        document.getElementById('right').style.marginLeft = "170px";
+        document.getElementById('sidebar-left').style.display = "flex";
     }else{
-        document.getElementById('right').style.marginLeft = '30px';
-        document.getElementById('sidebar-left').style.display = 'none';
+        document.getElementById('right').style.marginLeft = "30px";
+        document.getElementById('sidebar-left').style.display = "none";
     }
 }
