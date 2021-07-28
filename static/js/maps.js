c492ffe99ced5ef2731645475ddc0bd2b281bc2c
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-07-28 18:14:31 +0530

    Done

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,45 +1,20 @@
-var left_section = document.getElementById('left');
-var sidebar_right = document.getElementById('sidebar-right');
-var sidebar_left = document.getElementById('sidebar-left');
-var right_section = document.getElementById('right');
-
-left_section.onclick = function(){
-    if(sidebar_right.style.display == "none"){
-        left_section.style.marginRight = "370px";
-        sidebar_right.style.display = "block";
-    }else{
-        left_section.style.marginRight = "30px";
-        sidebar_right.style.display = "none";
-    }
-}
-
-right_section.onclick = function(){
-    if(sidebar_left.style.display == "none"){
-        right_section.style.marginLeft = "170px";
-        sidebar_left.style.display = "flex";
-    }else{
-        right_section.style.marginLeft = "30px";
-        sidebar_left.style.display = "none";
-    }
-}
-
 function ToggleFunction(){
-    if(sidebar_right.style.display == "none"){
-        left_section.style.marginRight = "370px";
-        sidebar_right.style.display = "block";
+    if(sidebar_right.style.display == 'none'){
+        document.getElementById('left').style.marginRight = '370px';
+        document.getElementById('sidebar-right').style.display = 'block';
     }else{
-        left_section.style.marginRight = "30px";
-        sidebar_right.style.display = "none";
+        document.getElementById('left').style.marginRight = '30px';
+        document.getElementById('sidebar-right').style.display = 'none';
     }
 }
 
 function RightToggleFunction(){
 
-    if(sidebar_left.style.display == "none"){
-        right_section.style.marginLeft = "170px";
-        sidebar_left.style.display = "flex";
+    if(document.getElementById('sidebar-left').style.display == 'none'){
+        document.getElementById('right').style.marginLeft = '170px';
+        document.getElementById('sidebar-left').style.display = 'flex';
     }else{
-        right_section.style.marginLeft = "30px";
-        sidebar_left.style.display = "none";
+        document.getElementById('right').style.marginLeft = '30px';
+        document.getElementById('sidebar-left').style.display = 'none';
     }
 }
