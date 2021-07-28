45911d4aa5fe8088ae391973f03b291767fe4c4a
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-07-28 17:51:36 +0530

    Done

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,23 +1,25 @@
-$(document).ready(function(){
-    $("#right").click(function(){
-        if($("#sidebar-left").css('display')=='none'){
-            $("#sidebar-left").css('display','flex');
-            $("#right").css('margin-right','170px');
-        }else{
-            $("#sidebar-left").css('display','none');
-            $("#right").css('margin-right','30px');
-        }
-    });
+var left_section = document.getElementById('left');
+var sidebar_right = document.getElementById('sidebar-right');
+var sidebar_left = document.getElementById('sidebar-left');
+var right_section = document.getElementById('right');
 
-    $("#left").click(function(){
-        if($("#sidebar-right").css('display')=='none'){
-            $("#sidebar-right").css('display','block');
-            $("#left").css('margin-left','370px');
-        }else{
-            $("#sidebar-right").css('display','none');
-            $("#left").css('margin-left','30px');
-        }
-    });
-});
+function ToggleFunction(){
+    if(sidebar_right.style.display === "none"){
+        left_section.style.marginRight = "370px";
+        sidebar_right.style.display = "block";
+    }else{
+        left_section.style.marginRight = "30px";
+        sidebar_right.style.display = "none";
+    }
+}
 
+function RightToggleFunction(){
 
+    if(sidebar_left.style.display === "none"){
+        right_section.style.marginLeft = "170px";
+        sidebar_left.style.display = "flex";
+    }else{
+        right_section.style.marginLeft = "30px";
+        sidebar_left.style.display = "none";
+    }
+}
