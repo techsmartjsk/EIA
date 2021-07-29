591c2f824f5db9693d988f666a463a3f6c1464eb
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-07-29 16:31:45 +0530

    Done

--- Routes/models.py
+++ Routes/models.py
@@ -4,4 +4,5 @@ from django.db.models.fields import TextField
 class Contents(models.Model):
     Heading = models.CharField(max_length=50)
     Content = models.TextField()
+    image = models.CharField(max_length=50, default="background.png")
 
