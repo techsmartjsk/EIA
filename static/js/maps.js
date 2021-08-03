9c42558f87e80bbb6159f2052873650c9422cd81
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-08-03 13:33:33 +0530

    Done

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,6 +1,6 @@
 function ToggleFunction(){
     if(document.getElementById('sidebar-right').style.display === "none"){
-        document.getElementById('left').style.marginRight = "370px";
+        document.getElementById('left').style.marginRight = "570px";
         document.getElementById('sidebar-right').style.display = "block";
     }else{
         document.getElementById('left').style.marginRight = "30px";
@@ -10,7 +10,7 @@ function ToggleFunction(){
 
 function RightToggleFunction(){
     if(document.getElementById('sidebar-left').style.display === "none"){
-        document.getElementById('right').style.marginLeft = "170px";
+        document.getElementById('right').style.marginLeft = "370px";
         document.getElementById('sidebar-left').style.display = "flex";
     }else{
         document.getElementById('right').style.marginLeft = "30px";
