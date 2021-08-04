8cb9670885b397e0c37ba8a96780f2bcb47b2a96
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-08-04 13:33:48 +0530

    Done

--- Routes/views.py
+++ Routes/views.py
@@ -61,6 +61,9 @@ def EIA(request):
     
     context = {}
 
+    context['chapters']= [{'name':'Abbreviations','link':'https://www.dropbox.com/s/jbipsfu9fq9qb4l/Sallachy-WF-Abbreviations.pdf?dl=0'},
+    {'name':'Introduction','link':'https://www.dropbox.com/scl/fi/z23j2hdfsi2wn5wbn8vej/Sallachy-WF-Chapter-1-Introduction.docx?dl=0&rlkey=la3pzfz39yswz5ou6htnijtr6'},]
+
     context['list'] = [{ 'id': 1,'heading':'Volume 1 : Chapters'},
     {'id': 2,'heading':'Volume 2 : Figures'},
     {'id': 3,'heading':'Volume 3 : LVIA NatureScot Figures'},
