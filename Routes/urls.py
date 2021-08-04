30919eefce192b1d986bcf0b4d4eae6813c576b3
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-08-04 15:23:56 +0530

    Done

--- Routes/urls.py
+++ Routes/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('maps/',views.maps,name='maps'),
     path('about/',views.about,name='about'),
     path('contents/<str:title>',views.contents,name='contents'),
-    path('EIA/',views.EIA,name='EIA'),
     path('volumes/<int:v_id>',views.Volume,name='Volumes'),
 ]
