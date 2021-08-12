e7b1f9166c12a8eaac7103b4ff8400370c6623be
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-08-12 15:50:12 +0530

    Done

--- static/js/maps.js
+++ static/js/maps.js
@@ -12,6 +12,9 @@ function RightToggleFunction(){
     if(document.getElementById('sidebar-left').style.display === "none"){
         document.getElementById('right').style.marginLeft = "370px";
         document.getElementById('sidebar-left').style.display = "flex";
+    }else if(document.getElementById('sidebar-left').style.display === "flex"){
+        document.getElementById('right').style.marginLeft = "30px";
+        document.getElementById('sidebar-left').style.display = "none";
     }else{
         document.getElementById('right').style.marginLeft = "30px";
         document.getElementById('sidebar-left').style.display = "none";
