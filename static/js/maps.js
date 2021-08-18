aa423d1efe0c7547fdd8e865172f6aa833e36837
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-08-18 21:22:03 +0530

    Done

--- static/js/maps.js
+++ static/js/maps.js
@@ -12,9 +12,15 @@ function RightToggleFunction(){
     if(document.getElementById('sidebar-left').style.display === "none"){
         document.getElementById('right').style.marginLeft = "28vw";
         document.getElementById('sidebar-left').style.display = "flex";
+        if(document.getElementById('left-icon').classList.contains === "fas fa-chevron-right"){
+            document.getElementById('left-icon').classList.remove = "fas fa-chevron-right";
+            document.getElementById('left-icon').classList.add = "fas fa-chevron-left";
+        }
     }else if(document.getElementById('sidebar-left').style.display === "flex"){
         document.getElementById('right').style.marginLeft = "3vw";
         document.getElementById('sidebar-left').style.display = "none";
+        document.getElementById('left-icon').classList.remove = "fas fa-chevron-left";
+        document.getElementById('left-icon').classList.add = "fas fa-chevron-right";
     }else{
         document.getElementById('right').style.marginLeft = "3vw";
         document.getElementById('sidebar-left').style.display = "none";
