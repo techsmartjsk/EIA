683f4b54ccb088d242ec7f66cb9886d583d272f7
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-08-18 21:37:46 +0530

    Done

--- static/js/maps.js
+++ static/js/maps.js
@@ -2,15 +2,15 @@ function ToggleFunction(){
     if(document.getElementById('sidebar-right').style.display === "none"){
         document.getElementById('left').style.marginRight = "51vw";
         document.getElementById('sidebar-right').style.display = "block";
-        if(document.getElementById('left-icon').classList.contains("fas fa-chevron-right")){
-            document.getElementById('left-icon').classList.remove("fas fa-chevron-right");
-            document.getElementById('left-icon').classList.add("fas fa-chevron-left");
+        if(document.getElementById('left-icon').classList.contains("fa-chevron-right")){
+            document.getElementById('left-icon').classList.remove("fa-chevron-right");
+            document.getElementById('left-icon').classList.add("fa-chevron-left");
         }
     }else{
         document.getElementById('left').style.marginRight = "3vw";
         document.getElementById('sidebar-right').style.display = "none";
-        document.getElementById('left-icon').classList.remove("fas fa-chevron-left");
-        document.getElementById('left-icon').classList.add("fas fa-chevron-right");
+        document.getElementById('left-icon').classList.remove("fa-chevron-left");
+        document.getElementById('left-icon').classList.add("fa-chevron-right");
     }
 }
 
