7559b9ec4170d9f9803c78dab1e52db2f41d0957
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-08-18 21:24:39 +0530

    Done

--- static/js/maps.js
+++ static/js/maps.js
@@ -2,9 +2,15 @@ function ToggleFunction(){
     if(document.getElementById('sidebar-right').style.display === "none"){
         document.getElementById('left').style.marginRight = "51vw";
         document.getElementById('sidebar-right').style.display = "block";
+        if(document.getElementById('left-icon').classList.contains === "fas fa-chevron-right"){
+            document.getElementById('left-icon').classList.remove = "fas fa-chevron-right";
+            document.getElementById('left-icon').classList.add = "fas fa-chevron-left";
+        }
     }else{
         document.getElementById('left').style.marginRight = "3vw";
         document.getElementById('sidebar-right').style.display = "none";
+        document.getElementById('left-icon').classList.remove = "fas fa-chevron-left";
+        document.getElementById('left-icon').classList.add = "fas fa-chevron-right";
     }
 }
 
@@ -12,15 +18,11 @@ function RightToggleFunction(){
     if(document.getElementById('sidebar-left').style.display === "none"){
         document.getElementById('right').style.marginLeft = "28vw";
         document.getElementById('sidebar-left').style.display = "flex";
-        if(document.getElementById('left-icon').classList.contains === "fas fa-chevron-right"){
-            document.getElementById('left-icon').classList.remove = "fas fa-chevron-right";
-            document.getElementById('left-icon').classList.add = "fas fa-chevron-left";
-        }
+        
     }else if(document.getElementById('sidebar-left').style.display === "flex"){
         document.getElementById('right').style.marginLeft = "3vw";
         document.getElementById('sidebar-left').style.display = "none";
-        document.getElementById('left-icon').classList.remove = "fas fa-chevron-left";
-        document.getElementById('left-icon').classList.add = "fas fa-chevron-right";
+        
     }else{
         document.getElementById('right').style.marginLeft = "3vw";
         document.getElementById('sidebar-left').style.display = "none";
