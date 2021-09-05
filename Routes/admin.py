63e050c5c0ae0143f03373c92452051acd63b8da
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-09-05 12:13:32 +0530

    Done

--- Routes/admin.py
+++ Routes/admin.py
@@ -1,9 +1,11 @@
 from Routes.models import Contents, Volumes
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from import_export.admin import ImportExportModelAdmin
 
 class ContentsAdmin(SummernoteModelAdmin):
     summernote_fields = ('Content',)
 
-admin.site.register(Contents,ContentsAdmin);
+admin.site.register(Contents,ImportExportModelAdmin)
+#admin.site.register(Contents,ContentsAdmin)
 admin.site.register(Volumes)
