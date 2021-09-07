5f06dc4c07f5408ce19bf11f4bc4e21e8bc3c4cb
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-09-07 14:03:29 +0530

    Done

--- Routes/models.py
+++ Routes/models.py
@@ -13,9 +13,15 @@ class Contents(models.Model):
     types = models.CharField(max_length=50,choices=types,default='image')
     image = models.CharField(max_length=250, default="background.png")
 
+    def __str__(self):
+        return self.Heading
+
 class Volumes(models.Model):
     name = models.CharField(max_length=50)
     link = models.CharField(max_length=350)
     volume = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
 
 
