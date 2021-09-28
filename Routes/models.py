48b583b66245a46021a5a46736ab22c342f11dbb
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2021-09-28 17:39:58 +0530

    Done

--- Routes/models.py
+++ Routes/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models.fields import TextField
 
 class Contents(models.Model):
-
     types = (
         ('MAP','map'),
         ('IMAGE','image'),
@@ -12,6 +11,7 @@ class Contents(models.Model):
     Content = models.TextField()
     types = models.CharField(max_length=50,choices=types,default='image')
     image = models.CharField(max_length=250, default="background.png")
+    #pos = models.IntegerField(default= 1)
 
     def __str__(self):
         return self.Heading
@@ -20,6 +20,7 @@ class Volumes(models.Model):
     name = models.CharField(max_length=50)
     link = models.CharField(max_length=350)
     volume = models.IntegerField(default=1)
+    #pos = models.IntegerField(default= 1)
 
     def __str__(self):
         return self.name
