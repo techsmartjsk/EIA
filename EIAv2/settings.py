a6340a1ab235ac3d156cc4881de249554de2284d
Jaskirat Singh <techsmartjsk@gmail.com>
2022-01-17 12:54:26 +0000

    Done

--- EIAv2/settings.py
+++ EIAv2/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'EIAv2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
