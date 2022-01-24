a75b8ba737d1732263d05482bcc90b72c03fb75e
Jaskirat Singh <jaskiratsingh@Jaskirats-MacBook-Air.local>
2022-01-24 20:07:11 +0530

    Done

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+function ShowPassword(){
+    var input = document.getElementById("password");
+    if(input.type == "password"){
+        input.type = "text";
+    }else{
+        input.type = "password";
+    }
+}
